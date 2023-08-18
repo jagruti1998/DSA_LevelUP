16d3681b2bed6290d27ae50df115887aa9bf8b71
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-18 18:38:30 +0530

    OneDVector

--- src/ArraysandVectors/OneDVector.java
+++ src/ArraysandVectors/OneDVector.java
@@ -1,4 +1,19 @@
 package ArraysandVectors;
+import java.util.Vector;
 
 public class OneDVector {
+
+    public static void main(String[] args) {
+
+
+        Vector<Integer> arr = new Vector<>();
+        arr.add(2);
+        arr.add(3);
+
+
+
+        for(int number : arr){
+            System.out.println(number);
+        }
+    }
 }

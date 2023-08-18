86ddf202a9641f08959516a6e05d56f8fd0ee7b6
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-18 18:42:48 +0530

    OneDVector

--- src/ArraysandVectors/OneDVector.java
+++ src/ArraysandVectors/OneDVector.java
@@ -5,13 +5,15 @@ public class OneDVector {
 
     public static void main(String[] args) {
 
-
+        // Create a new Vector named 'arr' to store integers
         Vector<Integer> arr = new Vector<>();
+
+        // Add elements (2 and 3) to the Vector
         arr.add(2);
         arr.add(3);
 
 
-
+        // Iterate through the elements in the Vector using enhanced for loop
         for(int number : arr){
             System.out.println(number);
         }

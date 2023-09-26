4deadc3d714c3c7c7c8ba0b4ad51e8ad0e754069
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-09-26 23:07:42 +0530

    Brute force

--- src/Sliding_Window_Problems/ContiguousSegmentsSum_BF.java
+++ src/Sliding_Window_Problems/ContiguousSegmentsSum_BF.java
@@ -1,2 +1,38 @@
-package Sliding_Window_Problems;public class fg {
+package Sliding_Window_Problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ContiguousSegmentsSum_BF {
+    public static List<int[]> findSegmentsWithSum(int[] arr, int k) {
+        List<int[]> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int currentSum = 0;
+            for (int j = i; j < arr.length; j++) {
+                currentSum += arr[j];
+                if (currentSum == k) {
+                    result.add(new int[]{i, j});
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 8;
+
+        List<int[]> segments = findSegmentsWithSum(arr, k);
+
+        if (segments.isEmpty()) {
+            System.out.println("No segments with sum " + k + " found.");
+        } else {
+            System.out.println("Segments with sum " + k + ":");
+            for (int[] segment : segments) {
+                System.out.println("[" + segment[0] + ", " + segment[1] + "]");
+            }
+        }
+    }
 }

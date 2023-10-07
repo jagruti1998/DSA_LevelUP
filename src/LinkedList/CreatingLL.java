1d44fd58392259c0984aee979b7f31af61938709
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-10-07 20:24:50 +0530

    CreatingLL

--- src/LinkedList/CreatingLL.java
+++ src/LinkedList/CreatingLL.java
@@ -1,4 +1,28 @@
 package LinkedList;
 
 public class CreatingLL {
+
+    //define a node class
+class Node{
+    int data;
+    Node next;
+
+        public Node(int data) { //constructor
+            this.data = data;//this refers to current node object
+            this.next = null;
+        }
+    }
+    Node head=null;//initially head is null
+
+
+
+    //Linked list
+    void insertAtHead(Node head, int data){
+
+
+    }
+
+public static void main(String args[]){
+
+}
 }

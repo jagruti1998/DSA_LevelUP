0d0b73affca894f3951d36cdfb1952d8fe2ffb0e
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-10-10 00:56:06 +0530

    CreatingLL

--- src/LinkedList/CreatingLL.java
+++ src/LinkedList/CreatingLL.java
@@ -2,27 +2,47 @@ package LinkedList;
 
 public class CreatingLL {
 
-    //define a node class
-class Node{
-    int data;
-    Node next;
+    // Define a Node class
+    class Node {
+        int data;
+        Node next;
 
-        public Node(int data) { //constructor
-            this.data = data;//this refers to current node object
+        public Node(int data) { // Constructor
+            this.data = data; // Set the data for the node
             this.next = null;
         }
     }
-    Node head=null;//initially head is null
 
+    Node head = null; // Initially, the head is null
 
-
-    //Linked list
-    void insertAtHead(Node head, int data){
-
-
+    // Insert a node at the head of the linked list
+    void insertAtHead(int data) {
+        Node newNode = new Node(data); // Create a new node with the given data
+        if (head == null) {
+            head = newNode; // If the list is empty, set the new node as the head
+        } else {
+            newNode.next = head; // Otherwise, set the new node's next to the current head
+            head = newNode; // Update the head to be the new node
+        }
     }
 
-public static void main(String args[]){
+    // Print the linked list
+    void printLL(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + "--");
+            current = current.next;
+        }
+        System.out.println();
+    }
 
-}
+    public static void main(String args[]) {
+        CreatingLL cl = new CreatingLL();
+        cl.insertAtHead(4); // Call the insertAtHead method on the cl object
+        cl.insertAtHead(3); // Call the insertAtHead method on the cl object
+        cl.insertAtHead(2); // Call the insertAtHead method on the cl object
+        cl.insertAtHead(1); // Call the insertAtHead method on the cl object
+        cl.insertAtHead(0); // Call the insertAtHead method on the cl object
+        cl.printLL(cl.head); // Call the printLL method to print the linked list
+    }
 }

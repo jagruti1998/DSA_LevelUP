7d8938228a79b56a2f9895b3d2d3f1565b4d3381
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-12-28 15:05:48 +0530

    commit

--- src/String/LongestPalindrome.java
+++ src/String/LongestPalindrome.java
@@ -4,21 +4,36 @@ import java.util.HashSet;
 
 public class LongestPalindrome {
     public int longestPalindrome(String s) {
+        // HashSet to store characters encountered
         HashSet<Character> set = new HashSet<>();
+
+        // Variable to track the length of the palindrome
         int len = 0;
 
+        // Iterate through each character in the string
         for (char c : s.toCharArray()) {
+
+            // Check if the character is already in the HashSet
             if (set.contains(c)) {
+
+                // If yes, it forms a pair for an even-length palindrome
                 len += 2;
-                set.remove(c);
+                set.remove(c); // Remove the character from the HashSet
             } else {
+
+                // If not, add the character to the HashSet
                 set.add(c);
             }
         }
 
+        // Check if there are characters left in the HashSet
         if (set.size() > 0) {
+
+            // If yes, there are characters that can be the center of an odd-length palindrome
             return len + 1;
         } else {
+
+            // If no characters left, return the length of the palindrome
             return len;
         }
     }

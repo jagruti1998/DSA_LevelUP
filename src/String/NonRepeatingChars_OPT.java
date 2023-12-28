a7a6ef2baffddfe79cbad5afcb18cfbdd7e30da8
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-12-28 08:43:30 +0530

    commit

--- src/String/NonRepeatingChars_OPT.java
+++ src/String/NonRepeatingChars_OPT.java
@@ -1,4 +1,36 @@
 package String;
 
+import java.util.HashMap;
+
 public class NonRepeatingChars_OPT {
+    public static void main(String[] args){
+        String s="abcd";
+        System.out.println(firstNonRepeatingChars(s));
+    }
+
+
+
+    public static int firstNonRepeatingChars(String s){
+
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i)))
+            {
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            }
+            else
+            {
+                map.put(s.charAt(i),1);
+            }
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(map.get(s.charAt(i))==1)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }

415c76270db081efd95fd6a864a5b656a408db33
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-12-28 13:08:37 +0530

    commit

--- src/String/LongestWord_BF.java
+++ src/String/LongestWord_BF.java
@@ -6,20 +6,33 @@ import java.util.HashSet;
 public class LongestWord_BF {
     public static void main(String[] args)
     {
+        // Input array of words
         String [] s = {"a","banana","app","appl","ap","apply","apple"};
+        // Call the method and print the result
         System.out.println(longestWord(s));
     }
 
-
+    // Method to find the longest word that can be constructed from other words
     public static String longestWord(String[] words){
+        // Sort the array of words in lexicographical order
         Arrays.sort(words);
+
+        // HashSet to store unique prefixes
         HashSet<String>set=new HashSet<>();
+
+        // Variable to store the longest word
         String longestWord="";
+
+        // Iterate through each word in the sorted array
         for(String word:words){
+
+            // Check if the word has a length of 1 or its prefix is in the HashSet
             if(word.length()==1   || set.contains(word.substring(0, word.length()-1)))
             {
+                // Add the word to the HashSet
                 set.add(word);
 
+                // Update the longest word if the current word is longer
                 if(word.length()>longestWord.length()){
                     longestWord=word;
                 }
@@ -27,7 +40,7 @@ public class LongestWord_BF {
 
         }
 
-
+        // Return the longest word
         return longestWord;
     }
 }

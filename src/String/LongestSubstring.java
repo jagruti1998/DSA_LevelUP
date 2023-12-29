7fb48256ddb310e88eb7869117dad525655ee4b2
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-12-29 08:27:42 +0530

    LongestSubstring

--- src/String/LongestSubstring.java
+++ src/String/LongestSubstring.java
@@ -9,6 +9,7 @@ public class LongestSubstring {
 
     }
     public static int lengthOfLongestSubstring(String s){
+        //HashSet to store unique characters
         HashSet< Character> set=new HashSet<>();
 
         int left=0,right=0;
@@ -16,19 +17,25 @@ public class LongestSubstring {
 
         while(right<s.length()-1)
         {
+
             if(!set.contains(s.charAt(right))){
+                //if character at right not in set , add to set
                 set.add(s.charAt(right));
+                //update max size if needed
                 max=Math.max(max,right-left+1);
+                //move right to next pointer
                 right++;
             }
 
             else
             {
+                //if character of right already in set, remove character at left
                 set.remove(s.charAt(left));
+                // Move 'left' pointer to the next position
                 left++;
             }
         }
-
+        // Return the length of the longest substring without repeating characters
         return max;
     }
 }
